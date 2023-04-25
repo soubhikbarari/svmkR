accdf663191b7c1bcc98730df47a7e5debb8cfa6
Soubhik Barari <soubhikbarari@gmail.com>
2023-04-25 16:10:10 -0400

    MERGED fixes with parsing in main into dev

--- R/fetch_survey.R
+++ R/fetch_survey.R
@@ -17,19 +17,19 @@ fetch_survey_obj <- function(id,
   if (missing(id)) {
     stop("specify an id")
   }
-
+  
   u <- "https://api.surveymonkey.com/v3/surveys?"
   h <- standard_request_header(oauth_token)
-
   p <- list("v3", survey = "surveys", id = id, details = "details")
-
+  
   parsed_content <- sm_get(url = u, query = NULL, config = h, path = p)
-
+  
   message("+ Parsing questions 📋")
-  families <- list()
-  questions <- list()
-  answers <- list()
-  levels <- list()
+  families  = list()
+  subtypes  = list()
+  questions = list()
+  answers   = list()
+  choices   = list()
   pbar <- utils::txtProgressBar(min=0, max=length(parsed_content))
   p <- 0
   while (p < length(parsed_content$pages)) {
@@ -37,38 +37,34 @@ fetch_survey_obj <- function(id,
     page <- parsed_content$pages[[p]]
     utils::setTxtProgressBar(pbar, p)
     for (question in page$questions) {
-      questions[[question$id]] <-
-        strip_tags(question$headings[[1]]$heading)
-      families[[question$id]] <- question$family
-      levels[[question$id]] <- c()
+      questions[[question$id]] <- strip_tags(question$headings[[1]]$heading)
+      families[[question$id]]  <- question$family
+      subtypes[[question$id]]  <- question$subtype
+      choices[[question$id]]   <- c()
+      
       if ("answers" %in% names(question)) {
         if ("rows" %in% names(question$answers)) {
           for (row in question$answers$rows) {
-            answers[[row$id]] <- trimws(
-              row$text
-            )
+            answers[[row$id]] <- trimws(row$text)
           }      
         }
         if ("choices" %in% names(question$answers)) {
           for (choice in question$answers$choices) {
-            answers[[choice$id]] <- trimws(
-              choice$text
-            )
-            levels[[question$id]] <- c(levels[[question$id]], answers[[choice$id]])
+            answers[[choice$id]] <- trimws(choice$text)
+            choices[[question$id]] <- c(choices[[question$id]], answers[[choice$id]])
           }
         }
         if ("other" %in% names(question$answers)) {
-          answers[[question$answers$other$id]] <- trimws(
-            question$answers$other$text
-          )
+          answers[[question$answers$other$id]] <- trimws(question$answers$other$text)
         }
       }
     }
   }
-  parsed_content$answers <- answers
-  parsed_content$questions <- questions
-  parsed_content$families <- families
-  parsed_content$levels <- levels
+  parsed_content$families   = families
+  parsed_content$subtypes   = subtypes
+  parsed_content$questions  = questions
+  parsed_content$answers    = answers
+  parsed_content$choices    = choices
   
   parsed_content
 }
