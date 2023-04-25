3fbf3f077e6feb917176124f3b3a0b59bd2c7cbe
Soubhik Barari <soubhikbarari@gmail.com>
2023-04-25 15:01:07 -0400

    updated get/post

--- R/request_utils.R
+++ R/request_utils.R
@@ -2,17 +2,48 @@ standard_request_header = function(token) {
   token = get_bearer_token(token)
   httr::add_headers(
     Authorization = token,
+    "Accept" = "application/json",
     "Content-Type" = "application/json"
   )
 }
 
-#' Get request for survey monkey API
+#' POST request for svmkR API
 #'
-#' Will always set the tntp/surveymonkey repo as the user agent
+#' Will always set the \code{soubhikbarari/svmkR} repo as the user agent
+#'
+#' @param url character, url to send request to
+#' @param body character, request body for POST call.
+#' @param config see \code{?httr::config}, \link[httr]{config}, for full details.
+#' Additional configuration settings such as http additional headers.
+#' @param ... additional argument passed to \code{httr::POST} \link[httr]{POST}.
+#' @export
+sm_post = function(url, body, config, ...) {
+  out = httr::POST(
+    url,
+    config = config,
+    body = body,
+    httr::user_agent("http://github.com/soubhikbarari/svmkR"),
+    ...
+  )
+  
+  remaining_request_message(out)
+  reset_time_message(out)
+  
+  out.parsed = httr::content(out, as = "parsed")
+  if ("error" %in% names(out.parsed)) {
+    stop(jsonlite::toJSON(out.parsed, pretty=TRUE))
+  } else {
+    out.parsed
+  }
+}
+
+#' GET request for svmkR API
+#'
+#' Will always set the \code{soubhikbarari/svmkR} repo as the user agent
 #'
 #' @param url character, url to send request to
 #' @param query character, components of the url to change for request arguments,
-#'  see also \code{?httr::modify_url}, \link[httr]{modify_url}
+#'  see also \code{?httr::modify_url}, \link[httr]{modify_url}.
 #' @param config see \code{?httr::config}, \link[httr]{config}, for full details.
 #' Additional configuration settings such as http additional headers.
 #' @param ... additional argument passed to \code{httr::GET} \link[httr]{GET}.
@@ -22,25 +53,28 @@ sm_get = function(url, query, config, ...) {
     url,
     config = config,
     query = query,
-    httr::user_agent("http://github.com/tntp/surveymonkey"),
+    httr::user_agent("http://github.com/soubhikbarari/svmkR"),
     ...
   )
-
-  httr::stop_for_status(out)
   
   remaining_request_message(out)
   reset_time_message(out)
   
-  httr::content(out, as = "parsed")
+  out.parsed = httr::content(out, as = "parsed")
+  if ("error" %in% names(out.parsed)) {
+    stop(jsonlite::toJSON(out.parsed, pretty=TRUE))
+  } else {
+    out.parsed
+  }
 }
 
 remaining_request_message = function(response) {
   if(length(response$headers$`x-ratelimit-app-global-day-remaining`>0)) {
-  message(paste0(
-    "You have ",
-    response$headers$`x-ratelimit-app-global-day-remaining`,
-    " requests left today before you hit the limit"
-  ))
+    message(paste0(
+      "You have ",
+      response$headers$`x-ratelimit-app-global-day-remaining`,
+      " requests left today before you hit the limit"
+    ))
   } else {
     warning("Could not determine API request limit")
   }
@@ -48,13 +82,13 @@ remaining_request_message = function(response) {
 
 reset_time_message = function(response, frequency = 20) {
   if(length(response$headers$`x-ratelimit-app-global-day-remaining`>0)) {
-  if (as.numeric(response$headers$`x-ratelimit-app-global-day-remaining`) %% frequency == 0) {
-    message(paste0(
-      "Your daily request limit will reset in ",
-      response$headers$`X-Ratelimit-App-Global-Day-Reset`,
-      " seconds"
-    ))
-  }
+    if (as.numeric(response$headers$`x-ratelimit-app-global-day-remaining`) %% frequency == 0) {
+      message(paste0(
+        "Your daily request limit will reset in ",
+        response$headers$`X-Ratelimit-App-Global-Day-Reset`,
+        " seconds"
+      ))
+    }
   } else {
     warning("Could not determine API request limit")
   }
